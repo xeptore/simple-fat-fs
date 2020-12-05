2041c945b3add83fbeef2295cb4adef920db5617
Morteza Behboodian <xeptore@gmail.com>
2020-12-05 11:34:19 +0330

    🌟 add formatter lib module
    containing functions helping to format partition file and directory file

---
+++ formatter.h
@@ -0,0 +1,5 @@
+// Formats partition on disk. Creates and initializes `partition.io` on disk.
+void format_partition();
+
+// Formats directory file on disk. Creates and initializes `directory.dir` on disk.
+void format_directory();

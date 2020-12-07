60ee854097bae81d1a46a1723da7b43c30357188
Morteza Behboodian <xeptore@gmail.com>
2020-12-07 11:43:24 +0330

    🔨 add 1 byte to directory entry byte size length
    used for null terminator

--- src/constants.h
+++ src/constants.h
@@ -7,7 +7,10 @@
 #define FAT_ENTRIES 1048576           // 2 ^ 20
 #define FAT_END_OF_FILE_ENTRY 1048577 // FAT_ENTRIES + 1
 #define FAT_EMPTY_ENTRY -1
-#define DIRECTORY_ENTRY_SIZE_IN_BYTES 23
+// 20 bytes for filename
+// 1 byte for filename null terminator
+// 3 bytes for fat entry index
+#define DIRECTORY_ENTRY_SIZE_IN_BYTES 24
 #define ROOT_DIRECTORY_MAX_FILES 32
 #define PARTITION_FILENAME "./partition.io"
 #define DIRECTORY_FILENAME "./directory.dir"

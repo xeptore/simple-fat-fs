39f9b21df1251409703b3d86cab85011d527274c
Morteza Behboodian <xeptore@gmail.com>
2020-12-16 17:32:25 +0330

    🌟 Add FAT_TABLE_SIZE_IN_BYTES constant

--- src/constants.h
+++ src/constants.h
@@ -4,9 +4,10 @@
 #define BLOCK_SIZE_IN_BYTES 1024
 #define MAX_FILENAME_LENGTH 20
 #define FAT_ENTRY_SIZE_IN_BITS 20
-#define FAT_ENTRIES 1048576           // 2 ^ 20
-#define FAT_END_OF_FILE_ENTRY 1048577 // 2 ^ 20 + 1
-#define FAT_EMPTY_ENTRY 1048578 // 2 ^ 20 + 2
+#define FAT_ENTRIES 1048576              // 2 ^ 20
+#define FAT_TABLE_SIZE_IN_BYTES 3145728  // (2 ^ 20) * 3
+#define FAT_END_OF_FILE_ENTRY 1048577    // 2 ^ 20 + 1
+#define FAT_EMPTY_ENTRY 1048578          // 2 ^ 20 + 2
 // 20 bytes for filename
 // 1 byte for filename null terminator
 // 3 bytes for fat entry index

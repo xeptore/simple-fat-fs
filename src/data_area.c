cf87def18eeab0250d2d4dd04c052f223b1398da
Morteza Behboodian <xeptore@gmail.com>
2020-12-19 11:06:20 +0330

    🌟 Add read_block_n and write_block_n to data_area

--- src/data_area.c
+++ src/data_area.c
@@ -1,4 +1,5 @@
 #include "constants.h"
+#include "types.h"
 #include <stdio.h>
 
 FILE* open_data_file() {
@@ -8,14 +9,27 @@ FILE* open_data_file() {
   return partition_file;
 }
 
-void save_block(
-  FILE* data_file,
-  const int index,
-  const unsigned char* block_data
+size_t read_block_n(
+  FILE* source_file,
+  const FatTableEntry block_index,
+  unsigned char* output_buffer
 ) {
   fpos_t before_seek_position;
-  fgetpos(data_file, &before_seek_position);
-  fseek(data_file, index * BLOCK_SIZE_IN_BYTES, SEEK_CUR);
-  fwrite(block_data, BLOCK_SIZE_IN_BYTES, 1, data_file);
-  fsetpos(data_file, &before_seek_position);
+  fgetpos(source_file, &before_seek_position);
+  fseek(source_file, block_index * BLOCK_SIZE_IN_BYTES, SEEK_CUR);
+  const size_t n_read_bytes = fread(output_buffer, BLOCK_SIZE_IN_BYTES, 1, source_file);
+  fsetpos(source_file, &before_seek_position);
+  return n_read_bytes;
+}
+
+void write_block_n(
+  FILE* destination_file,
+  const unsigned int block_index,
+  const unsigned char* block_buffer
+) {
+  fpos_t before_seek_position;
+  fgetpos(destination_file, &before_seek_position);
+  fseek(destination_file, block_index * BLOCK_SIZE_IN_BYTES, SEEK_CUR);
+  fwrite(block_buffer, BLOCK_SIZE_IN_BYTES, 1, destination_file);
+  fsetpos(destination_file, &before_seek_position);
 }

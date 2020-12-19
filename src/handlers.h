10012117a2128e8443a38e349d4c12e06e4a0dc7
Morteza Behboodian <xeptore@gmail.com>
2020-12-19 11:08:54 +0330

    🌟 Add handle_restore_file type declaration to handlers.h

--- src/handlers.h
+++ src/handlers.h
@@ -4,3 +4,4 @@
 int handle_new_filename(DirectoryFile* directory, FatTableEntry* fat_table);
 int handle_save_file(DirectoryFile* directory, FatTableEntry* fat_table);
 int handle_save_new_file(DirectoryFile* directory, FatTableEntry* fat_table);
+int handle_restore_file(DirectoryFile* directory, FatTableEntry* fat_table);
